8d6fca9070a1731c2b5f3bf379c940d343c41a58
Arghya Pal <arghya.1530@gmail.com>
2023-07-26 17:12:19 +0530

    1st commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// const end=new Date("30 August 2023 12:00:00 AM").getTime();
+const end=new Date().getTime();
+const inputs = document.querySelectorAll("input");
+function clocks() {
+    const now = new Date().getTime();
+    var distance = end - now;
+    if (distance <= 0) {
+        document.getElementById("show").style.display="block";
+        document.getElementById("form").style.display="none";
+    }
+    inputs[0].value = Math.floor(distance / (1000 * 60 * 60 * 24));
+    inputs[1].value = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    inputs[2].value = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    inputs[3].value = Math.floor((distance % (1000 * 60)) / 1000);
+  
+}
+clocks();
+setInterval(
+    () => {
+        clocks()
+    },1000
+)
+
